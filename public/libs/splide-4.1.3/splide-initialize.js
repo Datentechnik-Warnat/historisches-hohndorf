2cc96333eaad25641d128450605d21c1e565addd
Stefan Warnat <ich@stefanwarnat.de>
2025-01-29 15:05:13 +0100

    fix: remove public from repo

--- public/libs/splide-4.1.3/splide-initialize.js
+++
@@ -1,8 +0,0 @@
-var splideData;
-if (splideData) {
-  document.addEventListener("DOMContentLoaded", function (e) {
-    for (let id of splideData) {
-      new Splide("#" + id).mount();
-    }
-  });
-}
